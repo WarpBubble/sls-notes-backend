91bd8f86da60a171c5fb2d061dcb2d02edfc4069
WarpBubble <110014045+WarpBubble@users.noreply.github.com>
2023-03-25 22:29:13 +1300

    boilerplate for crud operations

---
+++ api/add-note.js
@@ -0,0 +1,32 @@
+//** Route: Post /note */
+
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'ap-southeast-2' })
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const tableName = process.env.NOTES_TABLE;
+
+const util = require('./utils')
+
+
+exports.handler = async (event) => {
+    try {
+
+        return {
+            statusCode: 200,
+            headers: util.getResponseHeaders();
+            body: JSON.stringify('')
+        }
+
+    } catch (err) {
+        console.log("Error", err)
+        return {
+            statusCode: err.statusCode ? err.StatusCode : 500,
+            headers: util.getResponseHeaders(),
+            body: JSON.stringify({
+                error: err.name ? err.name : "Exception",
+                message: err.message ? err.message : "Unkown error"
+            })
+        }
+    }
+}
